57e82f4d1da662bac3b35cdfe412395792971bf9
Ankur Lakhanpal <al0209@outlook.co.nz>
2019-09-26 22:22:21 +1200

    Added play/stop functionality
    added Play?stop Button to Tabla

--- ClassicalInstrumentApplication/ViewController.swift
+++ ClassicalInstrumentApplication/ViewController.swift
@@ -7,15 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+        var audioPlayer = AVAudioPlayer()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     
     }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
-
+    @IBAction func PlayTabla(_ sender: UIButton) {
+        
+        if sender.isSelected == false {
+        
+            sender.isSelected = true
+        // Set the sound file name & extension
+        let alertSound = URL(fileURLWithPath: Bundle.main.path(forResource: "Tabla", ofType: "wav")!)
+        
+        do {
+            // Preperation
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+        } catch _ {
+        }
+        do {
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch _ {
+        }
+        
+        // Play the sound
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: alertSound)
+        } catch _{
+        }
+        
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+    }
+    else {
+    
+            sender.isSelected = false
+    
+    audioPlayer.stop()
+        
+        }
+    }
 }
 
